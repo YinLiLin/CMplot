59236c152f5e1fe1c14ec71c7bb618947dd05157
Lilin Yin <1009926399@qq.com>
2020-05-10 17:18:46 +1000

    Update CMplot.r

--- R/CMplot.r
+++ R/CMplot.r
@@ -372,7 +372,7 @@ CMplot <- function(
         map <- as.matrix(map)
         map <- map[!is.na(map[, 2]), ]
         map <- map[!is.na(as.numeric(map[, 3])), ]
-        map <- map[map[, 2] != 0, ]
+        map <- map[as.numeric(map[, 2]) != 0, ]
         #map <- map[map[, 3] != 0, ]
         suppressWarnings(max.chr <- max(as.numeric(map[, 2]), na.rm=TRUE))
         if(is.infinite(max.chr))    max.chr <- 0
@@ -543,6 +543,9 @@ CMplot <- function(
         #order Pmap by the name of SNP
         #Pmap=Pmap[order(Pmap[,1]),]
         Pmap <- as.matrix(Pmap)
+        Pmap <- Pmap[!is.na(Pmap[, 2]), ]
+        Pmap <- Pmap[!is.na(as.numeric(Pmap[, 3])), ]
+        Pmap <- Pmap[as.numeric(Pmap[, 2]) != 0, ]
 
         #scale and adjust the parameters
         cir.chr.h <- cir.chr.h/5
@@ -1325,6 +1328,8 @@ CMplot <- function(
         is_visable <- list()
         for(i in 1:R){
             if(file.output){
+                ht=ifelse(is.null(height), 6, height)
+                wh=ifelse(is.null(width), 14, width)
                 is_visable[[i]] <- filter.points(pvalue.posN, logpvalueT[,i], wh, ht, dpi = dpi)
             }else{
                 is_visable[[i]] <- rep(TRUE, nrow(logpvalueT))
