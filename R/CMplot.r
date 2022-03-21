d7b20eee6a1257717b144c1d2ec85a28eeee2756
Lilin Yin <1009926399@qq.com>
2022-03-21 17:29:44 +0800

    fixed bug for p-value with NA

--- R/CMplot.r
+++ R/CMplot.r
@@ -735,7 +735,7 @@ CMplot <- function(
         if(band==0) band=1
         
         if(LOG10){
-            if(sum(pvalueT <= 0) != 0 || sum(pvalueT > 1) != 0) stop("p values should be at range of (0, 1).")
+            if(sum(pvalueT <= 0, na.rm = TRUE) != 0 || sum(pvalueT > 1, na.rm = TRUE) != 0) stop("p values should be at range of (0, 1).")
             pvalueT[pvalueT <= 0] <- NA
             pvalueT[pvalueT > 1] <- NA
         }
